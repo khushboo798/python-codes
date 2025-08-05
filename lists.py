8684820b61cc2283a01639e1d3e435441f47b5c2
khushboo798 <khushboothakral22@gmail.com>
2025-08-05 18:13:12 +0530

    updatedcode

--- lists.py
+++ lists.py
@@ -2,11 +2,39 @@
 # print(l)
 # print(type(l))
 
-marks =[3,5,6,"harry",True]
-print(marks)
-print(type(marks))
-print(marks[0])
-print(marks[1])
-print(marks[2])
-print(marks[3])
-print(marks[4])
+# marks =[3,5,6,"harry",True,32,7,9,1,2]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+# print(marks[-3])
+# print(marks[2])
+# print(marks[5-3])
+# print(marks[len(marks)-3])
+
+# if 6 in marks :
+#     print("yes")
+# else :
+#     print("no")
+
+# if "arry" in "harry":
+#     print("yes")
+
+# print(marks[1:-1])
+# print(marks[1:4])
+# print(marks[:4])
+# print(marks[1:4:2])
+# print(marks[::2])
+# print(marks[1:8:2])
+
+# lst =[ i for i in range(4)]
+# print(lst)
+# lst = [i*i for i in range(10)]
+# print(lst)
+# lst = [i*i for i in range(10) if i%2==0]
+# print(lst)
+
+l =[1,2 append ]
